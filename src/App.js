64d92e8f01cf82bc464ce6df11429db905caf974
zahafyou <madiop.niang@gmail.com>
2019-10-28 17:22:50 +0000

    my first Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const app = () => <h1>Hello GalsenTech</h1>
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <h1>{ this.props.text }</h1>
+      <p>je cherche une solution</p>
+      </>
+      )
+  }
 }
 
-export default App;
+
+export default App
